8ff1a6eda3db9eaf074f6326d53e2460a35fd788
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-08-20 20:29:55 +0600

    installer command

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -18,37 +18,51 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    public $description = 'added basic setting needed  for city bank install';
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    public $description = 'added basic setting needed for city bank install';
 
     /**
      * Execute the console command.
      *
      * @return int
      */
-    public function handle(): int
+    public function handle()
     {
-        if ($this->confirm('Do you wish to added Environment Variables?')) {
-            try {
-                return $this->addEnvironmentVariable();
-            } catch (\Exception $exception) {
-                $this->error($exception->getMessage());
+        if ($this->confirm('Publish Configuration File?')) {
+            $this->saveConfiguration();
+        }
 
-                return self::FAILURE;
-            }
+        if ($this->confirm('Do you wish to added Environment Variables?')) {
+            return $this->saveEnvironmentVariable();
         }
 
         return self::SUCCESS;
     }
 
     /**
+     * publish the configuration file to config directory
+     *
+     * @return void
+     */
+    protected function saveConfiguration()
+    {
+        if (file_exists(base_path('config/city-bank.php'))) {
+            if ($this->confirm('Overwrite this existing configuration file?')) {
+                $this->call('vendor:publish', ['--tag' => 'city-bank-config', '--force' => true]);
+                $this->info('Configuration published.');
+            }
+            $this->info('Configuration published cancelled.');
+        } else {
+            $this->call('vendor:publish', ['--tag' => 'city-bank-config']);
+            $this->info('Configuration published.');
+        }
+    }
+
+    /**
+     * Append Environment Variables to .env file
+     *
      * @return int
      */
-    protected function addEnvironmentVariable(): int
+    protected function saveEnvironmentVariable()
     {
         $envPath = base_path('.env');
 
@@ -56,36 +70,28 @@ class InstallCommand extends Command
 
             $envContent = file_get_contents($envPath);
 
-            $apiEnvVariableContent = '';
+            $apiEnvVariableContent = $this->envVariables();
 
-            if (strpos($envContent, "CITY_BANK_API_MODE") ||
-                strpos($envContent, "CITY_BANK_API_USERNAME") ||
-                strpos($envContent, "CITY_BANK_API_PASSWORD") ||
-                strpos($envContent, "CITY_BANK_EXCHANGE_COMPANY") ||
-                strpos($envContent, "CITY_BANK_API_HOST")) {
-
-                if ($this->confirm('Environment variables already exists try overwrite')) {
-                    $apiEnvVariableContent = $this->writeEnvVariables();
-                }
-
-                $this->error("Environment variables copy canceled");
-                return self::FAILURE;
-
-            } else {
-                $this->error("Environment variables update failed.");
-                return self::FAILURE;
+            if (file_put_contents($envPath, $envContent . $apiEnvVariableContent, FILE_APPEND) !== false) {
+                $this->error("Environment variables added successfully.");
+                return self::SUCCESS;
             }
-        } else {
-            $this->error("Can't find the (.env) file");
+
+            $this->error("Environment variables update failed.");
             return self::FAILURE;
         }
+
+        $this->error("Can't find the (.env) file");
+        return self::FAILURE;
     }
 
     /**
+     * Return the env values
+     *
      * @param bool $overwrite
      * @return string
      */
-    protected function writeEnvVariables(bool $overwrite = false): string
+    protected function envVariables($overwrite = false)
     {
         $currentConfig = config('city-bank');
 
@@ -94,18 +100,11 @@ class InstallCommand extends Command
         $password = '';
         $company = '';
 
-        if ($overwrite) {
-        }
-
-
-
-
-        $envContent = "
-            CITY_BANK_API_MODE=sandbox}\n
-            CITY_BANK_API_USERNAME=\n
-            CITY_BANK_API_PASSWORD=\n
-            CITY_BANK_EXCHANGE_COMPANY=\n
-            CITY_BANK_API_HOST=\n\n
-            ";
+        return "
+    CITY_BANK_API_MODE={$mode}\n
+    CITY_BANK_API_USERNAME={$username}\n
+    CITY_BANK_API_PASSWORD={$password}\n
+    CITY_BANK_EXCHANGE_COMPANY={$company}\n
+    ";
     }
 }
