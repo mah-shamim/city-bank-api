fcde558672319fbd1dfd003147df07f10e5c52da
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-08-28 15:54:09 +0600

    class condition updated

--- src/CityBank.php
+++ src/CityBank.php
@@ -4,22 +4,34 @@ namespace MahShamim\CityBank;
 
 use Exception;
 
+/**
+ * Class CityBank
+ *
+ * This class provides the details related to Remittance API.
+ * This APIs is used to initiate payment request from
+ * Mobile client/others exchange house.
+ *
+ * @property Config $config
+ * @property Request $request
+ *
+ * @package MahShamim\CityBank
+ */
 class CityBank
 {
     /**
      * @var Config
      */
-    public $config;
+    private $config;
 
     /**
      * @var Request
      */
-    public $request;
+    private $request;
 
     /**
      * CityBank constructor.
      *
-     * @param  array  $config
+     * @param array $config
      *
      * @throws Exception
      */
@@ -71,7 +83,7 @@ class CityBank
     }
 
     /**
-     * Authenticate service will provide you the access token by providing following parameter value.
+     * Do authenticate service will provide you the access token
      *
      * @return void
      *
@@ -120,7 +132,7 @@ class CityBank
     /**
      * Get transaction status service will help you to get the transaction status
      *
-     * @param  string  $reference
+     * @param string $reference
      * @return self
      *
      * @throws Exception
@@ -167,7 +179,7 @@ class CityBank
      * bKash customer validation service will help you to validate the beneficiary bkash number before send the transaction
      *
      * @param $mobileNumber
-     * @param  string  $fullName
+     * @param string $fullName
      * @return self
      *
      * @throws Exception
@@ -192,103 +204,66 @@ class CityBank
     /**
      * Do bKash transfer service will help you to send a bkash transaction
      *
-     * @param array
+     * @param string|integer $reference
+     * @param array $data
      * @return self
-     * @return mixed
-     *
      * @throws Exception
-     *
      * @since 2.1.0
      */
-    public function doBkashTransfer($data = [])
+    public function doBkashTransfer($reference, $data = [])
     {
-        $payload = [];
-        $payload['amount_in_bdt'] = isset($data['amount_in_bdt']) ? $data['amount_in_bdt'] : '?';
-        $payload['reference_no'] = isset($data['reference_no']) ? $data['reference_no'] : '?';
+        $payload = [
+            'reference_no' => $reference
+        ];
+
+        $payload['amount_in_bdt'] = isset($data['amount_in_bdt']) ? $data['amount_in_bdt'] : 0;
         $payload['remitter_name'] = isset($data['remitter_name']) ? $data['remitter_name'] : '?';
         $payload['remitter_dob'] = isset($data['remitter_dob']) ? $data['remitter_dob'] : '?';
-        $payload['remitter_iqama_no'] = isset($data['remitter_iqama_no']) ? $data['remitter_iqama_no'] : '?';
+
+        if (isset($data['remitter_iqama_no'])) {
+            $payload['remitter_iqama_no'] = $data['remitter_iqama_no'];
+        }
+
         $payload['remitter_id_passport_no'] = isset($data['remitter_id_passport_no']) ? $data['remitter_id_passport_no'] : '2';
-        $payload['remitter_address'] = isset($data['remitter_address']) ? $data['remitter_address'] : '?';
+
+        if (isset($data['remitter_address'])) {
+            $payload['remitter_address'] = $data['remitter_address'];
+        }
+
         $payload['remitter_mobile_no'] = isset($data['remitter_mobile_no']) ? $data['remitter_mobile_no'] : '?';
         $payload['issuing_country'] = isset($data['issuing_country']) ? $data['issuing_country'] : '?';
         $payload['beneficiary_name'] = isset($data['beneficiary_name']) ? $data['beneficiary_name'] : '?';
         $payload['beneficiary_city'] = isset($data['beneficiary_city']) ? $data['beneficiary_city'] : '?';
+
+        if (isset($data['beneficiary_id_no'])) {
+            $payload['beneficiary_id_no'] = $data['beneficiary_id_no'];
+            $payload['beneficiary_id_type'] = isset($data['beneficiary_id_type']) ? $data['beneficiary_id_type'] : '';
+        }
+
         $payload['beneficiary_id_no'] = isset($data['beneficiary_id_no']) ? $data['beneficiary_id_no'] : '?';
-        $payload['beneficiary_id_type'] = isset($data['beneficiary_id_type']) ? $data['beneficiary_id_type'] : '?';
         $payload['purpose_of_payment'] = isset($data['purpose_of_payment']) ? $data['purpose_of_payment'] : '?';
         $payload['beneficiary_mobile_phone_no'] = isset($data['beneficiary_mobile_phone_no']) ? $data['beneficiary_mobile_phone_no'] : '?';
-        $payload['beneficiary_address'] = isset($data['beneficiary_address']) ? $data['beneficiary_address'] : '?';
-        $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : '?';
+
+        if (isset($data['beneficiary_address'])) {
+            $payload['beneficiary_address'] = $data['beneficiary_address'];
+        }
+
+        $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : date('Y-m-d');
 
         $this->request
             ->method(Config::BKASH_TRANSFER)
             ->payload('do_bkash_transfer', $payload);
 
         return $this;
-
-        /*$doAuthenticate = $this->doAuthenticate();
-        if ($doAuthenticate != 'AUTH_FAILED' || $doAuthenticate != null):
-            $xml_string = '
-                <do_bkash_transfer xsi:type="urn:do_bkash_transfer">
-                    <!--You may enter the following 18 items in any order-->
-                    <token xsi:type="xsd:string">'.$doAuthenticate.'</token>
-                    <amount_in_bdt xsi:type="xsd:string">'.$inputData->transfer_amount.'</amount_in_bdt>
-                    <reference_no xsi:type="xsd:string">'.$inputData->reference_no.'</reference_no>
-                    <remitter_name xsi:type="xsd:string">'.$inputData->sender_first_name.'</remitter_name>
-                    <remitter_dob xsi:type="xsd:string">'.$inputData->sender_date_of_birth.'</remitter_dob>
-                    <!--Optional:-->
-                    <remitter_iqama_no xsi:type="xsd:string"/>
-                    <remitter_id_passport_no xsi:type="xsd:string">'.$inputData->sender_id_number.'</remitter_id_passport_no>
-                    <!--Optional:-->
-                    <remitter_address xsi:type="xsd:string">'.$inputData->sender_address.'</remitter_address>
-                    <remitter_mobile_no xsi:type="xsd:string">'.$inputData->sender_mobile.'</remitter_mobile_no>
-                    <issuing_country xsi:type="xsd:string">'.$inputData->sender_id_issue_country.'</issuing_country>
-            ';
-            if (isset($inputData->wallet_account_actual_name) && $inputData->wallet_account_actual_name != '') {
-                $xml_string .= '
-                    <beneficiary_name xsi:type="xsd:string">'.(isset($inputData->wallet_account_actual_name) ? $inputData->wallet_account_actual_name : null).'</beneficiary_name>
-            ';
-            } else {
-                $xml_string .= '
-                    <beneficiary_name xsi:type="xsd:string">'.((isset($inputData->receiver_first_name) ? $inputData->receiver_first_name : null).(isset($inputData->receiver_middle_name) ? ' '.$inputData->receiver_middle_name : null).(isset($inputData->receiver_last_name) ? ' '.$inputData->receiver_last_name : null)).'</beneficiary_name>
-            ';
-            }
-            $xml_string .= '
-                    <beneficiary_city xsi:type="xsd:string">'.(isset($inputData->receiver_city) ? $inputData->receiver_city : 'Dhaka').'</beneficiary_city>
-                    <!--Optional:-->
-                    <beneficiary_id_no xsi:type="xsd:string"></beneficiary_id_no>
-                    <!--Optional:-->
-                    <beneficiary_id_type xsi:type="xsd:string"></beneficiary_id_type>
-                    <purpose_of_payment xsi:type="xsd:string">'.$inputData->purpose_of_remittance.'</purpose_of_payment>
-                    <beneficiary_mobile_phone_no xsi:type="xsd:string">'.$inputData->bank_account_number.'</beneficiary_mobile_phone_no>
-                    <!--Optional:-->
-                    <beneficiary_address xsi:type="xsd:string">'.$inputData->receiver_address.'</beneficiary_address>
-                    <issue_date xsi:type="xsd:string">'.date('Y-m-d', strtotime($inputData->created_date)).'</issue_date>
-                </do_bkash_transfer>
-            ';
-            $soapMethod = 'doBkashTransfer';
-            $apiResponse = $this->connectionCheck($xml_string, $soapMethod);
-            if (isset($apiResponse) && $apiResponse != false && $apiResponse != null) {
-                $returnValue = json_decode($apiResponse->doBkashTransferResponse->Response, true);
-            } else {
-                $returnValue = ['message' => 'Transaction response Found', 'status' => 5000];
-            }
-        } else {
-            $returnValue = ['message' => 'AUTH_FAILED INVALID USER INFORMATION', 'status' => 103];
-        endif;
-        return $returnValue;*/
     }
 
     /**
      * This service call will provide you the bkash transaction status.
      *
-     * @param $inputData
-     * reference_no like system transaction number
+     * @param $reference
      * @return mixed
      *
      * @throws Exception
-     *
      * @since 2.1.0
      */
     public function getBkashTnxStatus($reference)
@@ -342,8 +317,8 @@ class CityBank
         $payload['mode_of_payment'] = isset($data['mode_of_payment']) ? $data['mode_of_payment'] : '';
         $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : '';
         for ($i = 1; $i <= 10; $i++) {
-            $payload['custom_field_name_'.$i] = isset($data['custom_field_name_'.$i]) ? $data['custom_field_name_'.$i] : '?';
-            $payload['custom_field_value_'.$i] = isset($data['custom_field_value_'.$i]) ? $data['custom_field_value_'.$i] : '?';
+            $payload['custom_field_name_' . $i] = isset($data['custom_field_name_' . $i]) ? $data['custom_field_name_' . $i] : '?';
+            $payload['custom_field_value_' . $i] = isset($data['custom_field_value_' . $i]) ? $data['custom_field_value_' . $i] : '?';
         }
         $this->request
             ->method(Config::TRANSFER)
