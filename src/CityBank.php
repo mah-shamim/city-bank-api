ced7d6c52eb851cb94f1323f14af5c071be23b47
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-08-28 16:13:23 +0600

    method management fixed

--- src/CityBank.php
+++ src/CityBank.php
@@ -110,21 +110,51 @@ class CityBank
     }
 
     /**
-     * Get balance service will help to know the available balance
+     * Do transfer service will help you to send a new transaction by providing following parameter value
      *
+     * @param $reference
+     * @param array $data
      * @return self
      *
      * @throws Exception
-     *
      * @since 2.0.0
      */
-    public function getBalance()
+    public function doTransfer($reference, $data = [])
     {
-        $payload = [];
+        $payload = ['reference_no' => $reference];
 
-        $this->request = $this->request
-            ->method(Config::BALANCE)
-            ->payload('get_balance', $payload);
+        $payload['remitter_name'] = isset($data['remitter_name']) ? $data['remitter_name'] : '';
+        $payload['remitter_code'] = isset($data['remitter_code']) ? $data['remitter_code'] : '';
+        $payload['remitter_iqama_no'] = isset($data['remitter_iqama_no']) ? $data['remitter_iqama_no'] : '';
+        $payload['remitter_id_passport_no'] = isset($data['remitter_id_passport_no']) ? $data['remitter_id_passport_no'] : '';
+        $payload['issuing_country'] = isset($data['issuing_country']) ? $data['issuing_country'] : '';
+        $payload['beneficiary_name'] = isset($data['beneficiary_name']) ? $data['beneficiary_name'] : '';
+        if (in_array($payload['mode_of_payment'], ['CBL Account', 'Other Bank'])) {
+            $payload['beneficiary_account_no'] = isset($data['beneficiary_account_no']) ? $data['beneficiary_account_no'] : '';
+            $payload['beneficiary_bank_account_type'] = isset($data['beneficiary_bank_account_type']) ? $data['beneficiary_bank_account_type'] : '';
+            $payload['beneficiary_bank_name'] = isset($data['beneficiary_bank_name']) ? $data['beneficiary_bank_name'] : '';
+            $payload['beneficiary_bank_branch_name'] = isset($data['beneficiary_bank_branch_name']) ? $data['beneficiary_bank_branch_name'] : '';
+            $payload['branch_routing_number'] = isset($data['branch_routing_number']) ? $data['branch_routing_number'] : '';
+        }
+        $payload['amount_in_bdt'] = isset($data['amount_in_bdt']) ? $data['amount_in_bdt'] : '';
+        $payload['purpose_of_payment'] = isset($data['purpose_of_payment']) ? $data['purpose_of_payment'] : '';
+        $payload['beneficiary_mobile_phone_no'] = isset($data['beneficiary_mobile_phone_no']) ? $data['beneficiary_mobile_phone_no'] : '?';
+        $payload['beneficiary_id_type'] = isset($data['beneficiary_id_type']) ? $data['beneficiary_id_type'] : '';
+        $payload['pin_no'] = isset($data['pin_no']) ? $data['pin_no'] : '';
+        $payload['remitter_address'] = isset($data['remitter_address']) ? $data['remitter_address'] : '';
+        $payload['remitter_mobile_no'] = isset($data['remitter_mobile_no']) ? $data['remitter_mobile_no'] : '';
+        $payload['beneficiary_address'] = isset($data['beneficiary_address']) ? $data['beneficiary_address'] : '';
+        $payload['beneficiary_id_no'] = isset($data['beneficiary_id_no']) ? $data['beneficiary_id_no'] : '';
+        $payload['special_instruction'] = isset($data['special_instruction']) ? $data['special_instruction'] : 'NA';
+        $payload['mode_of_payment'] = isset($data['mode_of_payment']) ? $data['mode_of_payment'] : '';
+        $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : '';
+        for ($i = 1; $i <= 10; $i++) {
+            $payload['custom_field_name_' . $i] = isset($data['custom_field_name_' . $i]) ? $data['custom_field_name_' . $i] : '?';
+            $payload['custom_field_value_' . $i] = isset($data['custom_field_value_' . $i]) ? $data['custom_field_value_' . $i] : '?';
+        }
+        $this->request
+            ->method(Config::TRANSFER)
+            ->payload('Transaction', $payload);
 
         return $this;
     }
@@ -163,14 +193,31 @@ class CityBank
      */
     public function doAmendmentOrCancel($reference, $details = '?')
     {
-        $payload = [
-            'reference_no' => $reference,
-            'amend_query' => $details,
-        ];
+        $payload = ['reference_no' => $reference, 'amend_query' => $details,];
 
         $this->request
             ->method(Config::AMENDMENT_OR_CANCEL)
             ->payload('txn_amend_cancel', $payload);
+
+        return $this;
+    }
+
+    /**
+     * Get balance service will help to know the available balance
+     *
+     * @return self
+     *
+     * @throws Exception
+     *
+     * @since 2.0.0
+     */
+    public function getBalance()
+    {
+        $payload = [];
+
+        $this->request = $this->request
+            ->method(Config::BALANCE)
+            ->payload('get_balance', $payload);
 
         return $this;
     }
@@ -202,7 +249,7 @@ class CityBank
     }
 
     /**
-     * Do bKash transfer service will help you to send a bkash transaction
+     * Do Bkash transfer service will help you to send a bkash transaction
      *
      * @param string|integer $reference
      * @param array $data
@@ -212,43 +259,45 @@ class CityBank
      */
     public function doBkashTransfer($reference, $data = [])
     {
-        $payload = [
-            'reference_no' => $reference
-        ];
+        $payload = ['reference_no' => $reference];
 
-        $payload['amount_in_bdt'] = isset($data['amount_in_bdt']) ? $data['amount_in_bdt'] : 0;
-        $payload['remitter_name'] = isset($data['remitter_name']) ? $data['remitter_name'] : '?';
-        $payload['remitter_dob'] = isset($data['remitter_dob']) ? $data['remitter_dob'] : '?';
+        try {
+            $payload['amount_in_bdt'] = isset($data['amount_in_bdt']) ? $data['amount_in_bdt'] : 0;
+            $payload['remitter_name'] = isset($data['remitter_name']) ? $data['remitter_name'] : '?';
+            $payload['remitter_dob'] = isset($data['remitter_dob']) ? $data['remitter_dob'] : '?';
 
-        if (isset($data['remitter_iqama_no'])) {
-            $payload['remitter_iqama_no'] = $data['remitter_iqama_no'];
+            if (isset($data['remitter_iqama_no'])) {
+                $payload['remitter_iqama_no'] = $data['remitter_iqama_no'];
+            }
+
+            $payload['remitter_id_passport_no'] = isset($data['remitter_id_passport_no']) ? $data['remitter_id_passport_no'] : '2';
+
+            if (isset($data['remitter_address'])) {
+                $payload['remitter_address'] = $data['remitter_address'];
+            }
+
+            $payload['remitter_mobile_no'] = isset($data['remitter_mobile_no']) ? $data['remitter_mobile_no'] : '?';
+            $payload['issuing_country'] = isset($data['issuing_country']) ? $data['issuing_country'] : '?';
+            $payload['beneficiary_name'] = isset($data['beneficiary_name']) ? $data['beneficiary_name'] : '?';
+            $payload['beneficiary_city'] = isset($data['beneficiary_city']) ? $data['beneficiary_city'] : '?';
+
+            if (isset($data['beneficiary_id_no'])) {
+                $payload['beneficiary_id_no'] = $data['beneficiary_id_no'];
+                $payload['beneficiary_id_type'] = isset($data['beneficiary_id_type']) ? $data['beneficiary_id_type'] : '';
+            }
+
+            $payload['beneficiary_id_no'] = isset($data['beneficiary_id_no']) ? $data['beneficiary_id_no'] : '?';
+            $payload['purpose_of_payment'] = isset($data['purpose_of_payment']) ? $data['purpose_of_payment'] : '?';
+            $payload['beneficiary_mobile_phone_no'] = isset($data['beneficiary_mobile_phone_no']) ? $data['beneficiary_mobile_phone_no'] : '?';
+
+            if (isset($data['beneficiary_address'])) {
+                $payload['beneficiary_address'] = $data['beneficiary_address'];
+            }
+
+            $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : date('Y-m-d');
+        } catch (\Exception $exception) {
+            throw new \Exception($exception->getMessage());
         }
-
-        $payload['remitter_id_passport_no'] = isset($data['remitter_id_passport_no']) ? $data['remitter_id_passport_no'] : '2';
-
-        if (isset($data['remitter_address'])) {
-            $payload['remitter_address'] = $data['remitter_address'];
-        }
-
-        $payload['remitter_mobile_no'] = isset($data['remitter_mobile_no']) ? $data['remitter_mobile_no'] : '?';
-        $payload['issuing_country'] = isset($data['issuing_country']) ? $data['issuing_country'] : '?';
-        $payload['beneficiary_name'] = isset($data['beneficiary_name']) ? $data['beneficiary_name'] : '?';
-        $payload['beneficiary_city'] = isset($data['beneficiary_city']) ? $data['beneficiary_city'] : '?';
-
-        if (isset($data['beneficiary_id_no'])) {
-            $payload['beneficiary_id_no'] = $data['beneficiary_id_no'];
-            $payload['beneficiary_id_type'] = isset($data['beneficiary_id_type']) ? $data['beneficiary_id_type'] : '';
-        }
-
-        $payload['beneficiary_id_no'] = isset($data['beneficiary_id_no']) ? $data['beneficiary_id_no'] : '?';
-        $payload['purpose_of_payment'] = isset($data['purpose_of_payment']) ? $data['purpose_of_payment'] : '?';
-        $payload['beneficiary_mobile_phone_no'] = isset($data['beneficiary_mobile_phone_no']) ? $data['beneficiary_mobile_phone_no'] : '?';
-
-        if (isset($data['beneficiary_address'])) {
-            $payload['beneficiary_address'] = $data['beneficiary_address'];
-        }
-
-        $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : date('Y-m-d');
 
         $this->request
             ->method(Config::BKASH_TRANSFER)
@@ -275,127 +324,5 @@ class CityBank
             ->payload('transaction_status', $payload);
 
         return $this;
-    }
-
-    /**
-     * Do transfer service will help you to send a new transaction by providing following parameter value
-     *
-     * @param $transferData
-     * @return self
-     *
-     * @throws Exception
-     *
-     * @since 2.0.0
-     */
-    public function transfer($transferData)
-    {
-        $payload = [];
-        $payload['reference_no'] = isset($data['reference_no']) ? $data['reference_no'] : '';
-        $payload['remitter_name'] = isset($data['remitter_name']) ? $data['remitter_name'] : '';
-        $payload['remitter_code'] = isset($data['remitter_code']) ? $data['remitter_code'] : '';
-        $payload['remitter_iqama_no'] = isset($data['remitter_iqama_no']) ? $data['remitter_iqama_no'] : '';
-        $payload['remitter_id_passport_no'] = isset($data['remitter_id_passport_no']) ? $data['remitter_id_passport_no'] : '';
-        $payload['issuing_country'] = isset($data['issuing_country']) ? $data['issuing_country'] : '';
-        $payload['beneficiary_name'] = isset($data['beneficiary_name']) ? $data['beneficiary_name'] : '';
-        if (in_array($payload['mode_of_payment'], ['CBL Account', 'Other Bank'])) {
-            $payload['beneficiary_account_no'] = isset($data['beneficiary_account_no']) ? $data['beneficiary_account_no'] : '';
-            $payload['beneficiary_bank_account_type'] = isset($data['beneficiary_bank_account_type']) ? $data['beneficiary_bank_account_type'] : '';
-            $payload['beneficiary_bank_name'] = isset($data['beneficiary_bank_name']) ? $data['beneficiary_bank_name'] : '';
-            $payload['beneficiary_bank_branch_name'] = isset($data['beneficiary_bank_branch_name']) ? $data['beneficiary_bank_branch_name'] : '';
-            $payload['branch_routing_number'] = isset($data['branch_routing_number']) ? $data['branch_routing_number'] : '';
-        }
-        $payload['amount_in_bdt'] = isset($data['amount_in_bdt']) ? $data['amount_in_bdt'] : '';
-        $payload['purpose_of_payment'] = isset($data['purpose_of_payment']) ? $data['purpose_of_payment'] : '';
-        $payload['beneficiary_mobile_phone_no'] = isset($data['beneficiary_mobile_phone_no']) ? $data['beneficiary_mobile_phone_no'] : '?';
-        $payload['beneficiary_id_type'] = isset($data['beneficiary_id_type']) ? $data['beneficiary_id_type'] : '';
-        $payload['pin_no'] = isset($data['pin_no']) ? $data['pin_no'] : '';
-        $payload['remitter_address'] = isset($data['remitter_address']) ? $data['remitter_address'] : '';
-        $payload['remitter_mobile_no'] = isset($data['remitter_mobile_no']) ? $data['remitter_mobile_no'] : '';
-        $payload['beneficiary_address'] = isset($data['beneficiary_address']) ? $data['beneficiary_address'] : '';
-        $payload['beneficiary_id_no'] = isset($data['beneficiary_id_no']) ? $data['beneficiary_id_no'] : '';
-        $payload['special_instruction'] = isset($data['special_instruction']) ? $data['special_instruction'] : 'NA';
-        $payload['mode_of_payment'] = isset($data['mode_of_payment']) ? $data['mode_of_payment'] : '';
-        $payload['issue_date'] = isset($data['issue_date']) ? $data['issue_date'] : '';
-        for ($i = 1; $i <= 10; $i++) {
-            $payload['custom_field_name_' . $i] = isset($data['custom_field_name_' . $i]) ? $data['custom_field_name_' . $i] : '?';
-            $payload['custom_field_value_' . $i] = isset($data['custom_field_value_' . $i]) ? $data['custom_field_value_' . $i] : '?';
-        }
-        $this->request
-            ->method(Config::TRANSFER)
-            ->payload('Transaction', $payload);
-
-        return $this;
-        /*if ($transferData->bank_id == 17) {
-            $mode_of_payment = 'CBL Account';
-        } else {
-            $mode_of_payment = 'Other Bank';
-        }
-        if ($transferData->recipient_type_name == 'Cash') {
-            $mode_of_payment = 'Cash';
-        }
-        if ($transferData->recipient_type_name == 'Cash Pickup') {
-            $mode_of_payment = 'Cash';
-        }
-        $xml_string = '<Transaction xsi:type="urn:Transaction">
-                    <reference_no xsi:type="xsd:string">'.$transferData->reference_no.'</reference_no>
-                    <remitter_name xsi:type="xsd:string">'.$transferData->sender_first_name.'</remitter_name>
-                    <remitter_code xsi:type="xsd:string">'.$transferData->sender_mobile.'</remitter_code>
-                    <remitter_iqama_no xsi:type="xsd:string"></remitter_iqama_no>
-                    <remitter_id_passport_no xsi:type="xsd:string">'.$transferData->sender_id_number.'</remitter_id_passport_no>
-                    <issuing_country xsi:type="xsd:string">'.$transferData->sender_id_issue_country.'</issuing_country>
-                    <beneficiary_name xsi:type="xsd:string">'.((isset($transferData->receiver_first_name) ? $transferData->receiver_first_name : null).(isset($transferData->receiver_middle_name) ? ' '.$transferData->receiver_middle_name : null).(isset($transferData->receiver_last_name) ? ' '.$transferData->receiver_last_name : null)).'</beneficiary_name>
-            ';
-        if ($mode_of_payment != 'Cash') {
-            $xml_string .= '
-                        <beneficiary_account_no xsi:type="xsd:string">'.$transferData->bank_account_number.'</beneficiary_account_no>
-                        <beneficiary_bank_account_type xsi:type="xsd:string">Savings</beneficiary_bank_account_type>
-                        <beneficiary_bank_name xsi:type="xsd:string">'.$transferData->bank_name.'</beneficiary_bank_name>
-                        <beneficiary_bank_branch_name xsi:type="xsd:string">'.$transferData->bank_branch_name.'</beneficiary_bank_branch_name>
-                        <branch_routing_number xsi:type="xsd:string">'.(isset($transferData->location_routing_id[1]->bank_branch_location_field_value) ? $transferData->location_routing_id[1]->bank_branch_location_field_value : null).'</branch_routing_number>
-                ';
-        }
-        $xml_string .= '<amount_in_taka xsi:type="xsd:string">'.$transferData->transfer_amount.'</amount_in_taka>
-                    <purpose_of_payment xsi:type="xsd:string">'.$transferData->purpose_of_remittance.'</purpose_of_payment>
-                    <beneficiary_mobile_phone_no xsi:type="xsd:string">'.$transferData->receiver_contact_number.'</beneficiary_mobile_phone_no>
-                    <beneficiary_id_type xsi:type="xsd:string"></beneficiary_id_type>
-                    <pin_no xsi:type="xsd:string"></pin_no>
-                    <remitter_address xsi:type="xsd:string">'.$transferData->sender_address.'</remitter_address>
-                    <remitter_mobile_no xsi:type="xsd:string">'.$transferData->sender_mobile.'</remitter_mobile_no>
-                    <beneficiary_address xsi:type="xsd:string">'.$transferData->receiver_address.'</beneficiary_address>
-                    <beneficiary_id_no xsi:type="xsd:string"></beneficiary_id_no>
-                    <special_instruction xsi:type="xsd:string">NA</special_instruction>
-                    <mode_of_payment xsi:type="xsd:string">'.$mode_of_payment.'</mode_of_payment>
-                    <issue_date xsi:type="xsd:string">'.date('Y-m-d', strtotime($transferData->created_date)).'</issue_date>
-                    <!--Optional:-->
-                    <custom_field_name_1 xsi:type="xsd:string">?</custom_field_name_1>
-                    <custom_field_value_1 xsi:type="xsd:string">?</custom_field_value_1>
-                    <custom_field_name_2 xsi:type="xsd:string">?</custom_field_name_2>
-                    <custom_field_value_2 xsi:type="xsd:string">?</custom_field_value_2>
-                    <custom_field_name_3 xsi:type="xsd:string">?</custom_field_name_3>
-                    <custom_field_value_3 xsi:type="xsd:string">?</custom_field_value_3>
-                    <custom_field_name_4 xsi:type="xsd:string">?</custom_field_name_4>
-                    <custom_field_value_4 xsi:type="xsd:string">?</custom_field_value_4>
-                    <custom_field_name_5 xsi:type="xsd:string">?</custom_field_name_5>
-                    <custom_field_value_5 xsi:type="xsd:string">?</custom_field_value_5>
-                    <custom_field_name_6 xsi:type="xsd:string">?</custom_field_name_6>
-                    <custom_field_value_6 xsi:type="xsd:string">?</custom_field_value_6>
-                    <custom_field_name_7 xsi:type="xsd:string">?</custom_field_name_7>
-                    <custom_field_value_7 xsi:type="xsd:string">?</custom_field_value_7>
-                    <custom_field_name_8 xsi:type="xsd:string">?</custom_field_name_8>
-                    <custom_field_value_8 xsi:type="xsd:string">?</custom_field_value_8>
-                    <custom_field_name_9 xsi:type="xsd:string">?</custom_field_name_9>
-                    <custom_field_value_9 xsi:type="xsd:string">?</custom_field_value_9>
-                    <custom_field_name_10 xsi:type="xsd:string">?</custom_field_name_10>
-                    <custom_field_value_10 xsi:type="xsd:string">?</custom_field_value_10>
-                </Transaction>';
-        $soapMethod = 'doTransfer';
-        $apiResponse = $this->connect($xml_string, $soapMethod);
-        if (isset($apiResponse) && $apiResponse != false && $apiResponse != null) {
-            $returnValue = json_decode($apiResponse->doTransferResponse->Response, true);
-        } else {
-            $returnValue = ['message' => 'Transaction response Found', 'status' => 5000];
-        }
-
-        return $returnValue;*/
     }
 }
